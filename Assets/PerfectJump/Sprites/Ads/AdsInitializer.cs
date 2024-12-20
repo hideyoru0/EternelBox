6eb613ad2c8ee58d434e7976ba422abab5e37d5a
hideyoru0 <hideyoru0@gmail.com>
2024-12-20 14:33:29 +0900

    adv pre done

---
+++ Assets/PerfectJump/Sprites/Ads/AdsInitializer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a0912b8523af5c6979adbaa1ea936c066a9eda5ffbb7d5b6f7a31930e81f9e5
+size 1106
---
+++ Assets/PerfectJump/Sprites/Ads/InterstitialAd.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d2f90375e8162d2f421fbcd39ecd8b8b45badc9c0cf13c0106583f5212d619c
+size 2167
